c3b89aa27ae04a99ae3a9e8a679ad05c393bcd83
Shakib202 <shakib2020360@gmail.com>
2022-04-18 16:12:41 +0600

    added login or register page

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,18 +1,31 @@
 import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { Link, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const Login = () => {
   const emailRef = useRef('');
   const passwordRef = useRef('');
   const navigate = useNavigate();
 
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  if(user){
+    navigate('/home');
+  }
+
   const handleSubmit = event => {
     event.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
 
-    console.log(email, password)
+    signInWithEmailAndPassword(email, password);
   }
 
   const navigateRegister = event => {
@@ -20,7 +33,7 @@ const Login = () => {
   }
 
   return (
-    <div className="container w-50 mx-auto mt-2">
+    <div className="container w-25 mx-auto mt-2">
       <h2 className="text-primary text-center">Please Login</h2>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
