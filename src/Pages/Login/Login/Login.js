54ac158dbfb970928e04dbb7870334bf2978e6ef
Shakib202 <shakib2020360@gmail.com>
2022-05-10 05:49:09 +0600

    added localStorage

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -11,6 +11,7 @@ import SocialLogin from "../SocialLogin/SocialLogin";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Helmet } from "react-helmet-async";
+import axios from "axios";
 
 const Login = () => {
   const emailRef = useRef("");
@@ -24,12 +25,15 @@ const Login = () => {
   const [signInWithEmailAndPassword, user, loading, error] =
     useSignInWithEmailAndPassword(auth);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
 
-    signInWithEmailAndPassword(email, password);
+    await signInWithEmailAndPassword(email, password);
+    const {data} = await axios.post('http://localhost:5000/login', {email});
+    localStorage.setItem('accessToken', data.accessToken);
+    navigate(from, { replace: true });
   };
 
   const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
@@ -39,7 +43,7 @@ const Login = () => {
   }
 
   if (user) {
-    navigate(from, { replace: true });
+    // navigate(from, { replace: true });
   }
 
   if (error) {
