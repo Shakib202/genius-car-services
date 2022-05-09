5b2f992761b2b06ee51af5ba273e06e56cf0f9a9
Shakib202 <shakib2020360@gmail.com>
2022-05-09 20:12:37 +0600

    hooks for chackout page

--- src/Pages/Chackout/Chackout/Chackout.js
+++ src/Pages/Chackout/Chackout/Chackout.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useServiceDetail from '../../../hooks/useServiceDetail';
+import './Chackout.css';
 
 const Chackout = () => {
+  const {serviceId} = useParams();
+  const [service] = useServiceDetail(serviceId);
   return (
-    <div>
-      <h2>Please Checkout your booking</h2>
+    <div className='w-50 mx-auto'>
+      <h2 className='text-center m-5 text-primary'>Please Order: {service.name}</h2>
+      <form>
+        <input type="text" name="name" placeholder='Name' id="" required />
+      </form>
     </div>
   );
 };
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,15 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import useServiceDetail from "../../hooks/useServiceDetail";
 
 const ServiceDetail = () => {
   const { serviceId } = useParams();
-  const [service, setService] = useState({});
-  useEffect(() => {
-    const url = `http://localhost:5000/service/${serviceId}`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setService(data));
-  }, []);
+  const [service] = useServiceDetail(serviceId);
   return (
     <div>
       <div className="text-center m-4">
@@ -17,7 +12,7 @@ const ServiceDetail = () => {
         <img style={{ borderRadius: "20px" }} src={service.img} alt="" />
       </div>
       <div className="text-center">
-        <Link to="/checkout">
+        <Link to={`/checkout/${serviceId}`}>
           <button className="btn btn-primary">Proceed Checkout</button>
         </Link>
       </div>
